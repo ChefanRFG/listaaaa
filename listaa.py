941a2d1a8f4fbd332dd939c782659951ef4eaf71
ChefanRFG <158303850+ChefanRFG@users.noreply.github.com>
2024-03-05 09:10:49 -0600

    Add files via upload

---
+++ listaa.py
@@ -0,0 +1,8 @@
+palabras = []
+for i in range(10):
+    palabra = input("Ingrese una palabra: ")
+    palabras.append(palabra)
+
+print("Las palabras ingresadas son:")
+for palabra in palabras:
+    print(palabra)
